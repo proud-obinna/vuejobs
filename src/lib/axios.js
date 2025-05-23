01059070d50e25d658432b906ef14084326ff12d
Obinna Okonkwo <quainnblink9@gmail.com>
2025-05-23 00:39:34 -0400

    updated axios url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/jobs',
+    baseURL: 'https://jsonserver-production-b98b.up.railway.app/jobs',
 });
 
 export default axiosInstance;
